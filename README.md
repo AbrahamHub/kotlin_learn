f34104f356e0e0c99b3056ec91feabb2d37731aa
AbrahamHub <apple_abraham@hotmail.com>
2021-04-26 12:15:15 -0500

    Semana-1

---
+++ README.md
@@ -0,0 +1,4 @@
+# Kotlin Learn (CBTis#42)
+<center><img width="180px" src="https://pbs.twimg.com/profile_images/3577239610/de22e09ba23793642657abd8c1075114.jpeg" alt="MDN"></center>
+
+# Branches
---
+++ main/kotlin/main.kt
@@ -0,0 +1,19 @@
+const val PI = 3.14159
+fun main(args: Array<String>) {
+    println("Hola mundo!")
+    var dinero = 10
+    println(dinero)
+    dinero = 5
+    println(dinero)
+    val nombre = "Abraham"
+    println(nombre)
+    println(PI)
+
+    //val boolean = true
+    //var numeroLargo = 1000000000000000L
+    //val double : Double = 2.1557
+    //val float  = 1.1F
+
+    val apellido = "Castañeda"
+    println("$nombre $apellido")
+}
