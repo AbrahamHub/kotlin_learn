95c042298c526774d56e1bffc666be45bf1313e2
AbrahamHub <apple_abraham@hotmail.com>
2021-04-25 19:49:53 -0500

    Variables 1

--- main/kotlin/main.kt
+++ main/kotlin/main.kt
@@ -1,3 +1,11 @@
+const val PI = 3.14159
 fun main(args: Array<String>) {
     println("Hola mundo!")
+    var dinero = 10
+    println(dinero)
+    dinero = 5
+    println(dinero)
+    val nombre = "Abraham"
+    println(nombre)
+    println(PI)
 }
