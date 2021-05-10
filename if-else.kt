ecc511a1467792987d6f2a913a40aeeebf3726da
Abraham Castañeda <59490517+AbrahamHub@users.noreply.github.com>
2021-05-10 12:39:52 -0500

    Create if-else.kt

---
+++ if-else.kt
@@ -0,0 +1,23 @@
+fun main() {
+// if-else &&
+
+    val num1 = 10
+    val num2 = 20
+
+    if(num1 > 0 && num2 > 0){
+        println("Ambos valores son mayores a 0")
+    } else{
+        println("Algun valor no es mayor a 0")
+    }
+
+    // if-else ||
+
+
+    if(num1 > 0 || num2 > 0){
+        println("Algun valor es mayor a 0")
+    } else{
+        println("Ambos valores no son mayores a 0)
+    }
+
+    val numeropositivo = if( num1 >= 0) println("El numero es positivo") else println("El numero es nagativo")
+}
