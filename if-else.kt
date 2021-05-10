5ea8a9e0c940a6ae8d442dddc4807f1999adc20e
Abraham Castañeda <59490517+AbrahamHub@users.noreply.github.com>
2021-05-10 12:53:54 -0500

    Update if-else.kt

--- if-else.kt
+++ if-else.kt
@@ -16,7 +16,7 @@ fun main() {
     if(num1 > 0 || num2 > 0){
         println("Algun valor es mayor a 0")
     } else{
-        println("Ambos valores no son mayores a 0)
+        println("Ambos valores no son mayores a 0")
     }
 
     val numeropositivo = if( num1 >= 0) println("El numero es positivo") else println("El numero es nagativo")
